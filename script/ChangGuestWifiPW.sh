2f1dbcf757c11b4d37f688ab43baa8154d5f6e6f
Markus Münzel <markus@muenzel.de>
2018-02-26 21:32:38 +0100

    removed extra pw file, use uci to get current guest pw

--- script/ChangGuestWifiPW.sh
+++ script/ChangGuestWifiPW.sh
@@ -1,18 +1,14 @@
 #!/bin/ash
-CHANGE=0
-SSID="ssid"
-GUEST_NETWORK_NAME="guest"
+GUEST_WIFI_SSID="ssid"
+
 PWD=`head /dev/urandom | tr -dc "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX!.,+#?*%&/()=" | head -c15`
 
-if [ `uci get wireless.@wifi-iface[2].network` = $GUEST_NETWORK_NAME ]; then
+# # assumes that guest wifi has id 2
+if [ `uci get wireless.@wifi-iface[2].ssid` = $GUEST_WIFI_SSID ]; then
   uci set wireless.@wifi-iface[2].key=$PWD
-  CHANGE=1
-fi
-if [ $CHANGE -eq 1 ]; then
   uci commit wireless
   wifi
-  QR_WIFI_STRING="WIFI:S:$SSID;T:WPA2;P:$PWD;;"
-  qrencode -t svg -l h -o /path/to/file/current_guest_wifi_pw_andorid_qr.svg $QR_WIFI_STRING
-  echo $PWD > /path/to/file/CurrentGuestPW.txt
+  QR_WIFI_STRING="WIFI:S:$GUEST_WIFI_SSID;T:WPA2;P:$PWD;;"
+  qrencode -t svg -l h -o /www/general-cgi-resources/current_guest_wifi_pw_andorid_qr.svg $QR_WIFI_STRING
 fi
 
